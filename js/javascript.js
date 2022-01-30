d9ae97990f6815da5aacc367e1fa93acb772b4c7
Dardlem <burmistrovartyom@gmail.com>
2022-01-30 18:14:37 +0200

    Added points display and calculate functions

--- js/javascript.js
+++ js/javascript.js
@@ -2,12 +2,37 @@ const computerPlayDisplay = document.getElementById("cPlayText");
 const playerPlayDisplay = document.getElementById("pPlayText");
 const playerRollHistoryDisplay = document.getElementById("playerRollHistory");
 const computerRollHistoryDisplay = document.getElementById("computerRollHistory");
+const playerPointsDisplay = document.getElementById("points player");
+const computerPointsDisplay = document.getElementById("points computer");
 
 let playerRollHistoryData = [];
 let computerRollHistoryData = [];
 
 let playerPoints = 0;
 let computerPoints = 0;
+
+const ruleList = new Map([
+    ["🪨", "✂️"],
+    ["✂️", "📄"],
+    ["📄", "🪨"],
+]);
+
+function calcPoins(valueOne, valueTwo){
+    switch (true){
+        case valueTwo === ruleList.get(valueOne):
+            playerPoints++;
+            break;
+        case valueOne === ruleList.get(valueTwo):
+            computerPoints++;
+            break;
+        default: console.log("calcPoints defaulted"); break;
+    }
+}
+
+function displayPoints(){
+    playerPointsDisplay.innerHTML = playerPoints;
+    computerPointsDisplay.innerHTML = computerPoints;
+}
 
 function playHand(a){
     if(a == 0){
@@ -22,9 +47,12 @@ function playHand(a){
 }
 
 function playRound(playerChoice){
-    playerPlayDisplay.innerHTML = playHand(playerChoice);
+    let playerDecision = playHand(playerChoice);
+    let computerDecision = handleComputerRound();
+    playerPlayDisplay.innerHTML = playerDecision;
     handleHistory(playerRollHistoryData, playerRollHistoryDisplay, playerPlayDisplay);
-    handleComputerRound();
+    calcPoins(playerDecision, computerDecision);
+    displayPoints();
 }
 
 function updateRollHistory(array, string){
@@ -50,8 +78,10 @@ function getRandomInt(max){
 }
 
 function handleComputerRound(){
-    computerPlayDisplay.innerHTML = playHand(getRandomInt(3));
+    let computerDecision = playHand(getRandomInt(3));
+    computerPlayDisplay.innerHTML = computerDecision;
     handleHistory(computerRollHistoryData, computerRollHistoryDisplay, computerPlayDisplay);
+    return computerDecision;
 }
 
 function playerBtnRock(){
